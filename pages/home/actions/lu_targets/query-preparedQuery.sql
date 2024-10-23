99c8fb5d24553ea2d034a4b021effce32c465db1
UI Bakery <chris.bates@spbridge.co.uk>
2024-10-23 11:57:06 +0000

    Remade action that must have been corrupted

--- pages/home/actions/lu_targets/query-preparedQuery.sql
+++ pages/home/actions/lu_targets/query-preparedQuery.sql
@@ -1,3 +1,16 @@
-select lptc.id as id, lptc.topic_id, lptc.level, lptc.category,lptc.target, lptc.subject_id,lptc.pillar_id, ptc.topic, ptc.pillar, CONCAT(ptc.pillar, ': ', ptc.topic) AS display from public.lu_pillar_target_cache lptc
-join public.lu_pillar_topic_cache ptc on lptc.topic_id = ptc.id 
-where lptc.topic_id in (?)
+select
+  lptc.id as id,
+  lptc.topic_id,
+  lptc.level,
+  lptc.category,
+  lptc.target,
+  lptc.subject_id,
+  lptc.pillar_id,
+  ptc.topic,
+  ptc.pillar,
+  CONCAT (ptc.pillar, ': ', ptc.topic) AS display
+from
+  public.lu_pillar_target_cache lptc
+  join public.lu_pillar_topic_cache ptc on lptc.topic_id = ptc.id
+where
+  lptc.topic_id in (?)
